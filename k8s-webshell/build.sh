6f38173b0f0a7e5a448d38cd2a6d16c80a7d647f
makai-fit2cloud <makai@fit2cloud.com>
2022-11-07 11:08:34 +0800

    修改默认端口

--- k8s-webshell/build.sh
+++ k8s-webshell/build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 mkdir bin
+echo "start package"
 GOOS=linux go build -o bin/k8s-webshell
 #GOOS=darwin go build -o bin/k8s-webshell-mac
 #GOOS=windows go build -o bin/k8s-webshell.exe
 
 # Copy
-cp k8s-webshell bin/
-cp conf bin/
-cp static bin/
-cp views bin/
+echo "start copy"
+pwd
+cp -rf conf bin/
+cp -rf static bin/
+cp -rf views bin/
+cp Dockerfile bin/
 
 # Build
+echo "start build"
 cd bin/
+pwd
 docker build -t registry.fit2cloud.com/north/k8s-webshell:master .
 docker push registry.fit2cloud.com/north/k8s-webshell:master
 
